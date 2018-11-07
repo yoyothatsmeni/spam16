4d70cb6c6478bef3ce365eea87033295a8c7ccec
yoyothatsmeni <44788338+yoyothatsmeni@users.noreply.github.com>
2018-11-08 00:46:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,25 +23,5 @@ client.on('message', message => {
     }
     });
 
-client.on('ready',async () => {
-  let GUILDID = '497726385637556236';
-  let CHANNELID = '509857256976678933'; 
-  voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
-
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
-
-    if(channel.type === 'voice') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
-    }
-  }
-});
 
 client.login(process.env.TOKEN);
