19f2d3826a09ff22c5ad76f633389b84813903d8
yoyothatsmeni <44788338+yoyothatsmeni@users.noreply.github.com>
2018-11-13 16:22:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1064,5 +1064,58 @@ if (message.content.startsWith(prefix + 'setplay')) {
 
 });
 
+client.on('message', async message => {
+  var prefix = "f!"
+  if(message.content.startsWith(prefix + "buy")) {
+    await message.channel.send("**عمرك**").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.delete();
+     message.channel.send('**المنتج**').then(m => {
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        m.delete();
+message.channel.send('**اسم الاونر الي تبي تشتري منه**').then(ms => {
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+
+ms.delete()
+
+ message.channel.send('جاري التقديم').then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم تسليمك في اسرع وقت ممكن**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'تسليم')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
+.addField('     أسم العضو ' ,` ${message.author.tag} `)
+.addField('     العمر ' ,`  \n ${lan} \ `)
+.addField('     المنتج ' ,`  \n ${md} \ `)
+.addField('     الاونر ' ,` \n ${br} \ `)
+.setColor('RANDOM')
+          .setFooter(`FireShop`)
+.setTimestamp()
+});
+}        
+})
+})
+})
+})
+})
+})
+})
+ }
+})
 
 client.login(process.env.BOT_KEY);
